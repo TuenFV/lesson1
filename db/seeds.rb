f9c6d59abfc9f7a2c768acc05c87090dc4b82a7f
LoKi <sand5088@gmail.com>
2022-02-20 00:14:22 +0700

    init model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+  c = Category.new
+  c.name = Faker::Name.name
+  c.description = Faker::Company.industry
+  c.save
+end
+
+Category.all.each do |cate|
+  5.times do |t|
+  	p = cate.products.new
+	p.category_id = cate.id
+	p.name = Faker::Name.name
+	p.description = Faker::Company.industry
+	p.stock = Faker::Number.within(range: 10..100)
+	p.price = Faker::Number.within(range: 1000..10000)
+	p.save
+  end
+end
