76ead05504894366d353952f2c3ffccaaa2d8e04
-LoKi- <44358997+TuenFV@users.noreply.github.com>
2022-02-22 21:15:17 +0700

    Category controller (#2)
    * Controller lesson
    
    * Update follow comments pull rq1
    
    * update follow comments
    
    * update 2 after comments
    
    * update 3

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,56 @@
+class Admin::CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :show, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params_category)
+    
+    if @category.save
+      flash[:notice] = 'Category was successfully created!'
+      redirect_to admin_category_path(@category)
+    else
+      flash[:alert] = @category.errors.full_messages
+      redirect_to new_admin_category_path
+      debugger
+    end
+    
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(params_category)
+      flash[:notice] = "Category was sucessfully updated!"
+      redirect_to admin_category_path
+    else
+      flash[:alert] = @category.errors.full_messages
+      redirect_to edit_admin_category_path
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = "Category was sucessfully destroyed!"
+    redirect_to admin_categories_path
+  end
+
+  private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def params_category
+      params_category = params.require(:category).permit(:name, :description)
+    end
+end
