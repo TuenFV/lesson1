4de445e3712b9b8e37eee8a2c8c2af79a7dadc4a
LoKi <sand5088@gmail.com>
2022-02-23 21:55:29 +0700

    update UI category

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.2-2/lib/assets/compiled/rails-ujs.js"
